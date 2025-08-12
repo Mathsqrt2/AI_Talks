bcb9ec17bc16a1a7bcb0805bb54ae90031ca1e73
Mathsqrt2 <micho.bugajski@gmail.com>
2025-08-12 23:46:45 +0200

    new database entrypoint

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,59 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 14, 2025 at 07:01 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ai_talks`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comment`
---
-
-CREATE TABLE `comment` (
+CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
-  `conversationId` int(11) NOT NULL,
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL,
+  `conversationId` int(11) DEFAULT NULL,
   `mode` varchar(128) NOT NULL,
   `botId` int(11) NOT NULL,
-  `username` varchar(256) NOT NULL,
-  `createdAt` bigint(20) NOT NULL
+  `username` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `conversation`
---
-
-CREATE TABLE `conversation` (
+CREATE TABLE `conversations` (
   `id` int(11) NOT NULL,
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL,
   `conversationName` varchar(512) NOT NULL,
-  `initialPrompt` text NOT NULL,
-  `createdAt` bigint(20) NOT NULL
+  `initialPrompt` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `logs`
---
 
 CREATE TABLE `logs` (
   `id` int(11) NOT NULL,
@@ -62,31 +31,26 @@ CREATE TABLE `logs` (
   `label` varchar(256) DEFAULT NULL,
   `tag` varchar(128) DEFAULT NULL,
   `conversationId` int(11) DEFAULT NULL,
-  `createdAt` bigint(20) NOT NULL
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `message`
---
-
-CREATE TABLE `message` (
+CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
-  `conversationId` int(11) NOT NULL,
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL,
+  `conversationId` int(11) DEFAULT NULL,
   `author` varchar(32) NOT NULL,
   `content` text NOT NULL,
   `generationTime` bigint(20) NOT NULL,
-  `generatingStartTime` bigint(20) NOT NULL,
-  `generatingEndTime` bigint(20) NOT NULL,
-  `createdAt` bigint(20) NOT NULL
+  `generatingStartTime` datetime NOT NULL,
+  `generatingEndTime` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `settings`
---
 
 CREATE TABLE `settings` (
   `id` int(11) NOT NULL,
@@ -95,150 +59,125 @@ CREATE TABLE `settings` (
   `maxContextSize` int(11) DEFAULT NULL,
   `maxAttempts` int(11) DEFAULT NULL,
   `retryAfterTimeInMiliseconds` int(11) DEFAULT NULL,
-  `createdAt` bigint(20) NOT NULL
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `state`
---
-
-CREATE TABLE `state` (
+CREATE TABLE `states` (
   `id` int(11) NOT NULL,
-  `conversationId` int(11) NOT NULL,
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL,
+  `conversationId` int(11) DEFAULT NULL,
   `shouldContinue` tinyint(4) DEFAULT NULL,
   `shouldSendToTelegram` tinyint(4) DEFAULT NULL,
   `shouldDisplayResponse` tinyint(4) DEFAULT NULL,
+  `shouldBroadcastOnWebSocket` tinyint(4) DEFAULT NULL,
   `shouldLog` tinyint(4) DEFAULT NULL,
   `isGeneratingOnAir` tinyint(4) DEFAULT NULL,
   `lastResponderName` varchar(256) DEFAULT NULL,
   `enqueuedMessageContent` text DEFAULT NULL,
   `enqueuedMessageAuthor` varchar(256) DEFAULT NULL,
-  `currentMessageIndex` int(11) NOT NULL,
-  `createdAt` bigint(20) NOT NULL
+  `currentMessageIndex` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
+CREATE TABLE `summaries` (
+  `id` int(11) NOT NULL,
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL,
+  `conversationId` int(11) DEFAULT NULL,
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for table `comment`
---
-ALTER TABLE `comment`
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `createdAt` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `updatedAt` datetime(6) DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `initializationHash` varchar(128) DEFAULT NULL,
+  `currentHash` varchar(128) DEFAULT NULL,
+  `login` varchar(256) NOT NULL,
+  `password` varchar(512) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `users` (`id`, `createdAt`, `updatedAt`, `initializationHash`, `currentHash`, `login`, `password`) VALUES
+(1, '2025-08-12 22:39:15.545191', '2025-08-12 22:44:58.965325', 'b42e56493afdd26cacb4023e0076b44112c2d3d5b1152b67f97dd27e91d420c09b4e5229376a50873d0af4c5baee8384cb596346e23c993ff35cea6a048540b7', 'b42e56493afdd26cacb4023e0076b44112c2d3d5b1152b67f97dd27e91d420c09b4e5229376a50873d0af4c5baee8384cb596346e23c993ff35cea6a048540b7', 'ai_talks', 'e03ebc90-f536-487d-b0a7-04ea821e61e8$022b70b9ba0347c4edde012e1f065fddba6f40a16ccfa8875d057dcd339f2189c0eac8ed7135dfdb5ede29c6d75dff934c1ba377b6321021c3967941f9d9eed2');
+
+ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_84d5faee520938f1faffd3363ac` (`conversationId`);
+  ADD KEY `FK_2ba3f4e3ed6ec82a45bfd008a98` (`conversationId`);
 
---
--- Indexes for table `conversation`
---
-ALTER TABLE `conversation`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `conversations`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `IDX_574180ce3486911f2b268b8473` (`conversationName`);
 
---
--- Indexes for table `logs`
---
 ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK_7af773c87422ef99cddbcc39067` (`conversationId`);
 
---
--- Indexes for table `message`
---
-ALTER TABLE `message`
+ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_7cf4a4df1f2627f72bf6231635f` (`conversationId`);
+  ADD KEY `FK_e5663ce0c730b2de83445e2fd19` (`conversationId`);
 
---
--- Indexes for table `settings`
---
 ALTER TABLE `settings`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK_2f7c413efd45151bf555934514b` (`conversationId`);
 
---
--- Indexes for table `state`
---
-ALTER TABLE `state`
+ALTER TABLE `states`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_6e79526e45e88a4edd6da84f694` (`conversationId`);
+  ADD KEY `FK_98d084feeac4c6d13b5eab91760` (`conversationId`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+ALTER TABLE `summaries`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_49fc33d364036bd554b2693d7d9` (`conversationId`);
 
---
--- AUTO_INCREMENT for table `comment`
---
-ALTER TABLE `comment`
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `IDX_2d443082eccd5198f95f2a36e2` (`login`);
+
+ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `conversation`
---
-ALTER TABLE `conversation`
+ALTER TABLE `conversations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `logs`
---
 ALTER TABLE `logs`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `message`
---
-ALTER TABLE `message`
+ALTER TABLE `messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `settings`
---
 ALTER TABLE `settings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `state`
---
-ALTER TABLE `state`
+ALTER TABLE `states`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
+ALTER TABLE `summaries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for table `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `FK_84d5faee520938f1faffd3363ac` FOREIGN KEY (`conversationId`) REFERENCES `conversation` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
---
--- Constraints for table `logs`
---
+ALTER TABLE `comments`
+  ADD CONSTRAINT `FK_2ba3f4e3ed6ec82a45bfd008a98` FOREIGN KEY (`conversationId`) REFERENCES `conversations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 ALTER TABLE `logs`
-  ADD CONSTRAINT `FK_7af773c87422ef99cddbcc39067` FOREIGN KEY (`conversationId`) REFERENCES `conversation` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK_7af773c87422ef99cddbcc39067` FOREIGN KEY (`conversationId`) REFERENCES `conversations` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
---
--- Constraints for table `message`
---
-ALTER TABLE `message`
-  ADD CONSTRAINT `FK_7cf4a4df1f2627f72bf6231635f` FOREIGN KEY (`conversationId`) REFERENCES `conversation` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `messages`
+  ADD CONSTRAINT `FK_e5663ce0c730b2de83445e2fd19` FOREIGN KEY (`conversationId`) REFERENCES `conversations` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
---
--- Constraints for table `settings`
---
 ALTER TABLE `settings`
-  ADD CONSTRAINT `FK_2f7c413efd45151bf555934514b` FOREIGN KEY (`conversationId`) REFERENCES `conversation` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK_2f7c413efd45151bf555934514b` FOREIGN KEY (`conversationId`) REFERENCES `conversations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `state`
---
-ALTER TABLE `state`
-  ADD CONSTRAINT `FK_6e79526e45e88a4edd6da84f694` FOREIGN KEY (`conversationId`) REFERENCES `conversation` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `states`
+  ADD CONSTRAINT `FK_98d084feeac4c6d13b5eab91760` FOREIGN KEY (`conversationId`) REFERENCES `conversations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `summaries`
+  ADD CONSTRAINT `FK_49fc33d364036bd554b2693d7d9` FOREIGN KEY (`conversationId`) REFERENCES `conversations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
